6b31e13ea719a77e740c27762dba26d4ab5458ca
unknown <c.leplumey@gmail.com>
2024-03-26 22:50:54 +0100

    WIP

--- scenes/collectable.gd
+++ scenes/collectable.gd
@@ -3,6 +3,7 @@ extends Area2D
 @onready var game_manager = %GameManager
 
 func _on_body_entered(body):
+	print("test")
 	if(body.name == "CharacterBody2D"):
-		game_manager.add_points()
+		#game_manager.add_points()
 		queue_free()
