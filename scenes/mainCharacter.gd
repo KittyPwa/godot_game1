836d22482365f093a1b7efcb592548fd37385a28
unknown <c.leplumey@gmail.com>
2024-03-31 20:02:08 +0200

    added level 4 + wall_jumping

--- scenes/mainCharacter.gd
+++ scenes/mainCharacter.gd
@@ -4,9 +4,11 @@ var can_move = true
 const SPEED_X = 400.0
 const SLOW_DOWN = 40.0
 const JUMP_VELOCITY = -730.0
+const WALL_JUMP_EXTRA_VELOCITY = -365.0
 var jump_amount = 0
 var animation_name = ""
 var was_on_floor = false
+var wall_jumped = false
 @onready var coyote_timer = %coyoteTimer
 
 @onready var sprite_2d = $Sprite2D
@@ -34,23 +36,15 @@ func _physics_process(delta):
 		if(jump_amount == 2):
 			animation_name = "double_jump"
 		sprite_2d.animation = animation_name
+		
+		var direction_x = Input.get_axis("left", "right")
+		
 		if not is_on_floor():
 			velocity.y += gravity * delta	
 		else:
 			jump_amount = 0
-		# Handle jump.
-		if Input.is_action_just_pressed("jump"):
-			if is_on_floor() or !coyote_timer.is_stopped():
-				velocity.y = JUMP_VELOCITY
-				jump_amount = 1
-			else:
-				if jump_amount == 1:
-					velocity.y = JUMP_VELOCITY
-					jump_amount = 2
-
-		# Get the input direction and handle the movement/deceleration.
-		# As good practice, you should replace UI actions with custom gameplay actions.
-		var direction_x = Input.get_axis("left", "right")
+			wall_jumped = false
+		
 		if direction_x:
 			velocity.x = direction_x * SPEED_X
 		else:
@@ -58,6 +52,31 @@ func _physics_process(delta):
 			
 		if was_on_floor and !is_on_floor():
 			coyote_timer.start()
+		if is_on_ceiling():
+			wall_jumped = false
+			jump_amount = 1
+		# Handle jump.
+		if Input.is_action_just_pressed("jump"):
+			var do_jump = false
+			var extra_jump_power = 0
+			if is_on_floor() or !coyote_timer.is_stopped():
+				do_jump = true
+				jump_amount = 1
+			elif is_on_wall_only() && !wall_jumped:
+				extra_jump_power = WALL_JUMP_EXTRA_VELOCITY
+				do_jump = true				
+				wall_jumped = true
+				jump_amount = 1
+			elif jump_amount == 1:
+				do_jump = true				
+				jump_amount = 2
+			if do_jump:
+				velocity.y = JUMP_VELOCITY + extra_jump_power
+			
+
+		# Get the input direction and handle the movement/deceleration.
+		# As good practice, you should replace UI actions with custom gameplay actions.
+		
 		
 		was_on_floor = is_on_floor()
 		
